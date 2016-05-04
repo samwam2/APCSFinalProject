5767a761555744e1021791f52bab172de0625725
Sam Showalter <samwam2@gmail.com>
2016-05-04 09:16:35 -0700

    First Commit
    First Commit

---
+++ apcsFinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apcsFinalProject
+//
+//  Created by ShowalterS18 on 5/4/16.
+//  Copyright © 2016 Sam Showalter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
