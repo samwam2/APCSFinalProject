22b053691e7968ae165850e3cf95b53b47e37e04
Sam Showalter <samwam2@gmail.com>
2016-05-04 21:04:37 -0700

    Test Code
    Starting test basic iOS things
    
    Sam
    Showalter

--- apcsFinalProject/ViewController.swift
+++ apcsFinalProject/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //var emojiArray: [String] = []
+    var emojiArray: String = ""
+    
 
+    @IBOutlet var textFeild: UITextField!
+    @IBAction func TestButton(sender: AnyObject) {
+        
+        getText()
+        printcharterBycharter()
+        //print(emojiArray)
+        print("finshed")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+      
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func printcharterBycharter() {
+        
+        var i = 0
+        
+        for _ in 1...5 {
+            i++
+            print(emojiArray.startIndex)
+            print(emojiArray)
+            print(i)
+        }
+    }
+    
+    func getText() {
+        emojiArray = textFeild.text!
+    }
 
 }
 
