7a6bd28afeaad76d6c672a12e4ee0b7fdc817db6
Sam Showalter <samwam2@gmail.com>
2016-05-10 15:19:48 -0700

    Somewhat beta version
    Have made a somewhat bete version that works with one emoji per user input. More to come

--- apcsFinalProject/ViewController.swift
+++ apcsFinalProject/ViewController.swift
@@ -12,44 +12,71 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //var emojiArray: [String] = []
     var emojiArray: String = ""
+    var textOnScreen = ""
     
 
     @IBOutlet var textFeild: UITextField!
     @IBAction func buttonTest(sender: AnyObject) {
         getText()
         print(emojiArray)
-     
-        
         print("finshed")
-        
+    //getText.()
+        convertedtextisHERE.text = textOnScreen
+        compare()
     }
     
-     @IBOutlet internal var convertedToTextHere: UITextField!
     
-//    var text: String = ""
-//    func putText(text: String) -> String {
-//        var place = text
-//        place = convertedToTextHere.placeholder!
-//        
-//        return text
-//    }
+    @IBOutlet var convertedtextisHERE: UILabel!
+     @IBOutlet internal var convertedToTextHere: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
       convertedToTextHere.delegate = self
         
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("Memory worning, FIX SAM")
     }
-
+    
+    
+    
   
     func getText() {
         emojiArray = textFeild.text!
+       // textFeild.
+    }
+    var compareText:String = ""
+    
+    func compare() {
+        
+      //emojiArray.startIndex
+
+      //  var indexInt: int = 0
+//        while emojiArray.isEmpty == false {
+//            
+//        }
+        
+        switch emojiArray {
+    case "😀":
+        textOnScreen = "Happy Face"
+    case "😬":
+            textOnScreen = "Ahhhh..."
+    case "😁":
+            textOnScreen = "Smilling ahhh face"
+    case "😂":
+                textOnScreen = "Laughing so hard I amd crying"
+    case "😃":
+                textOnScreen = " happy face 2"
+    case " hi":
+                textOnScreen = "hi"
+    default:
+        textOnScreen = "no input or Error"
+}
+        
+       
     }
 
 }
